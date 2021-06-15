a6b382f2242473b34d8cd0c5c5beae7881e468d3
ryfoa6 <duckduckgoose384@gmail.com>
2021-06-15 09:14:46 -0700

    Add Task model and controller

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -11,10 +11,10 @@ router.get('/', auth, async (req, res) => {
     }
     try {
         if(!filters){
-            const foundTasks = await Tasks.find({});
+            const foundTasks = await Task.find({});
             res.status(200).json(foundTasks)
         } else {
-            const foundTasks = await Tasks.find({...filters});
+            const foundTasks = await Task.find({...filters});
             res.status(200).json(foundTasks)
         }  
     }catch(error){
@@ -26,7 +26,7 @@ router.get('/', auth, async (req, res) => {
 // Create
 router.post('/', auth, async (req, res) => {
     try {
-        const createdTasks = await Tasks.create(req.body)
+        const createdTasks = await Task.create(req.body)
         res.status(200).json(createdTasks)
     } catch(err){
         res.status(400).json({
@@ -37,7 +37,7 @@ router.post('/', auth, async (req, res) => {
 // Read
 router.get('/:id', auth, async (req, res) => {
     try {
-        const foundTasks = await Tasks.findById(req.params.id);
+        const foundTasks = await Task.findById(req.params.id);
         res.status(200).json(foundTasks)
     } catch (error) {
         res.status(400).json({
@@ -48,7 +48,7 @@ router.get('/:id', auth, async (req, res) => {
 // Update
 router.put('/:id', auth, async (req, res) => {
     try {
-        const updatedTasks = await Tasks.findByIdAndUpdate(req.params.id, req.body, { new: true } )
+        const updatedTasks = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true } )
         res.status(200).json(updatedTasks);
     } catch (error) {
         res.status(400).json({
@@ -59,7 +59,7 @@ router.put('/:id', auth, async (req, res) => {
 // Delete
 router.delete('/:id', auth, async (req, res) => {
     try {
-        const deletedTasks = await Tasks.findByIdAndDelete(req.params.id);
+        const deletedTasks = await Task.findByIdAndDelete(req.params.id);
         res.status(200).json(deletedTasks);
     } catch (error) {
         res.status(400).json({
